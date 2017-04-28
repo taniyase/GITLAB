054658195a607e18aecd89be6aeec1e872b4a582
taniyase <ttaniyasee@gmail.com>
2017-04-29 01:07:46 +0700

    c program

---
+++ program2.c
@@ -0,0 +1,21 @@
+//program to find Factors of a Positive Integer
+
+#include <stdio.h>
+int main()
+{
+    int number, i;
+
+    printf("Enter a positive integer: ");
+    scanf("%d",&number);
+
+    printf("Factors of %d are: ", number);
+    for(i=1; i <= number; ++i)
+    {
+        if (number%i == 0)
+        {
+            printf("%d ",i);
+        }
+    }
+
+    return 0;
+}
